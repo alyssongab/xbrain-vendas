4afc507b34c9d50c4f03c2a5f472cdb7514c486a
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-25 18:46:51 -0400

    fix: add validation annotations to venda request dto

--- src/main/java/com/xbrain/vendas/domain/VendaRequestDto.java
+++ src/main/java/com/xbrain/vendas/domain/VendaRequestDto.java
@@ -1,12 +1,25 @@
 package com.xbrain.vendas.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record VendaRequestDto(
+        @NotNull
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate dataVenda,
+
+        @NotNull
         BigDecimal valorVenda,
+
+        @Positive
         int idVendedor,
+
+        @NotBlank
         String nomeVendedor
 ) {
 }

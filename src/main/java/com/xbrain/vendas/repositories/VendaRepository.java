4dfe33e6dca40faedd449e273b432f538a6f6899
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-27 17:49:19 -0400

    feat: add services to add venda and get vendedores with the stats asked

--- src/main/java/com/xbrain/vendas/repositories/VendaRepository.java
+++ src/main/java/com/xbrain/vendas/repositories/VendaRepository.java
@@ -4,10 +4,12 @@ import com.xbrain.vendas.domain.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
     List<Venda> findByDataVendaBetween(LocalDate start, LocalDate end);
 
--- src/main/java/com/xbrain/vendas/services/VendaService.java
+++ src/main/java/com/xbrain/vendas/services/VendaService.java
@@ -2,8 +2,17 @@ package com.xbrain.vendas.services;
 
 import com.xbrain.vendas.domain.Venda;
 import com.xbrain.vendas.dto.VendaDto;
+import com.xbrain.vendas.dto.VendedorDto;
 import com.xbrain.vendas.repositories.VendaRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class VendaService {
@@ -22,6 +31,37 @@ public class VendaService {
 
         this.vendaRepository.save(venda);
         return convertToResponse(venda);
+    }
+
+    // lista de vendedores com as estatisticas (total $ de vendas e media diaria de vendas) no periodo
+    public List<VendedorDto> getVendedores(LocalDate dataInicio, LocalDate dataFim) {
+        var vendasNoPeriodo = this.vendaRepository.findByDataVendaBetween(dataInicio, dataFim);
+
+        long diasNoPeriodo = ChronoUnit.DAYS.between(dataInicio, dataFim) + 1;
+
+        Map<String, List<Venda>> vendasPorVendedor =  vendasNoPeriodo.stream()
+                .collect(Collectors.groupingBy(Venda::getNomeVendedor));
+
+        return vendasPorVendedor.entrySet().stream()
+            .map(entry -> {
+                String nomeVendedor = entry.getKey();
+                List<Venda> vendasVendedor = entry.getValue();
+
+                BigDecimal totalVendas = vendasVendedor.stream()
+                        .map(Venda::getValorVenda)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+                BigDecimal mediaDiariaVendas = totalVendas
+                        .divide(BigDecimal.valueOf(diasNoPeriodo), 2, RoundingMode.HALF_UP);
+
+                return new VendedorDto(
+                        nomeVendedor,
+                        totalVendas,
+                        mediaDiariaVendas
+                );
+            })
+            .collect(Collectors.toList());
+
     }
 
     private VendaDto.Response convertToResponse(Venda venda) {

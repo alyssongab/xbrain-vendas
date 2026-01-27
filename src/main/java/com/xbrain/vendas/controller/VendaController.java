b77354a02e0e8cef464b0e62f261e0a385b7defc
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-27 17:49:45 -0400

    feat: add controller to call service methods

--- src/main/java/com/xbrain/vendas/controller/VendaController.java
+++ src/main/java/com/xbrain/vendas/controller/VendaController.java
@@ -1,15 +1,16 @@
 package com.xbrain.vendas.controller;
 
-import com.xbrain.vendas.domain.Venda;
 import com.xbrain.vendas.dto.VendaDto;
+import com.xbrain.vendas.dto.VendedorDto;
 import com.xbrain.vendas.services.VendaService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/vendas")
@@ -24,5 +25,12 @@ public class VendaController {
     public ResponseEntity<VendaDto.Response> createSale(@RequestBody @Valid VendaDto.Request request) {
         VendaDto.Response vendaResponse = this.vendaService.addVenda(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(vendaResponse);
+    }
+
+    @GetMapping("/vendedores")
+    public ResponseEntity<List<VendedorDto>> getVendedores(@RequestParam("data_inicio") LocalDate dataInicio,
+                                                           @RequestParam("data_fim") LocalDate dataFim) {
+        List<VendedorDto> vendedores = this.vendaService.getVendedores(dataInicio, dataFim);
+        return ResponseEntity.ok(vendedores);
     }
 }
